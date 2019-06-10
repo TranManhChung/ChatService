0ee64c7dd5e783d095dfe76aea434114747e7861
TranManhChung <TranManhChung>
2019-06-10 07:53:52 +0700

    add test

--- ChatMicroservice/auth-service/src/test/java/com/vng/authservice/TestAuthServiceImpl.java
+++ ChatMicroservice/auth-service/src/test/java/com/vng/authservice/TestAuthServiceImpl.java
@@ -2,7 +2,12 @@ package com.vng.authservice;
 
 import com.vng.authservice.service.AuthServiceImpl;
 import org.junit.*;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TestAuthServiceImpl{
 
     @Test
--- ChatMicroservice/ui-web-app/src/test/java/com/vng/uiwebapp/UiWebAppApplicationTests.java
+++ ChatMicroservice/ui-web-app/src/test/java/com/vng/uiwebapp/UiWebAppApplicationTests.java
@@ -2,16 +2,14 @@ package com.vng.uiwebapp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+@RunWith(Suite.class)
+@Suite.SuiteClasses({GrpcClientTest.class})
 public class UiWebAppApplicationTests {
-
-    @Test
-    public void contextLoads() {
-
-    }
 
 }
